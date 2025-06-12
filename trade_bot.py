7e8e7c6672466e06a3dbead54934e7160870fa71
ntp-k <natthaphat41@gmail.com>
2025-06-13 00:46:03 +0700

    🚀 add macd trading bot

--- trade_bot.py
+++ trade_bot.py
@@ -1,9 +1,10 @@
 import macd_simlulate_trade
+import macd_bot_trade
 import time
 
 def main():
     while True:
-        macd_simlulate_trade.main()
+        macd_bot_trade.main()
         time.sleep(30)
 
 if __name__ == "__main__":
