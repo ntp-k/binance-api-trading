187fc6b71983a304dc03f0302065471cfd8f24e7
ntpk <natthaphat.kiti@gmail.com>
2025-06-15 21:51:47 +0700

    ⚡ update azure adapter

--- data_adapters/azure_sql_adapter.py
+++ data_adapters/azure_sql_adapter.py
@@ -1,14 +1,13 @@
-
-import pyodbc
-import os
 import pyodbc
 import os
 from dotenv import load_dotenv
 
+from commons.custom_logger import CustomLogger
 from base_adapter import BaseAdapter
 
 # Fetch variables
 load_dotenv()
+
 server = os.getenv("AZURE_SQL_DB_SERVER")
 database = os.getenv("AZURE_SQL_DB_DATABASE")
 username = os.getenv("AZURE_SQL_DB_HOME_PC_USER")
@@ -18,28 +17,60 @@ connection_string = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};D
 
 class AzureSQLAdapter(BaseAdapter):
     def __init__(self):
-        self.conn = pyodbc.connect(os.getenv("AZURE_SQL_CONN"))
+        self.logger = CustomLogger(name=self.__class__.__name__)
 
-    def fetch_bot_configs(self):
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT * FROM BotConfigs WHERE is_active=1")
-        rows = cursor.fetchall()
-        # return list of dicts with keys like 'mode', 'strategy', etc.
-        return None
+        try:
+            self.connection_string = os.getenv("AZURE_SQL_CONN") or self.build_conn_string()
+            self.conn = pyodbc.connect(self.connection_string)
+            self.logger.info("Successfully connected to Azure SQL database.")
+        except Exception as e:
+            self.logger.error(f"Failed to connect to Azure SQL: {e}")
+            raise
+
+
+    def build_conn_string(self):
+        server = os.getenv("AZURE_SQL_DB_SERVER", 'Not Set')
+        database = os.getenv("AZURE_SQL_DB_DATABASE", 'Not Set')
+        username = os.getenv("AZURE_SQL_DB_HOME_PC_USER", 'Not Set')
+        password = os.getenv("AZURE_SQL_DB_ADMIN_PASSWORD", 'Not Set')
+
+        if any(val == 'Not Set' for val in [server, database, username, password]):
+            raise OSError("Missing one or more Azure SQL DB environment variables.")
+        
+        return f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+
+    def fetch_bot_configs(self) -> list:
+        try:
+            self.logger.info("Fetching active bot configs from Azure SQL...")
+            cursor = self.conn.cursor()
+
+            cursor.execute("SELECT * FROM [binance-bot-db].bnb.bot_configs WHERE enabled=1;")
+            columns = [column[0] for column in cursor.description]
+            rows = cursor.fetchall()
+            configs = [dict(zip(columns, row)) for row in rows]
+            self.logger.info(f"Retrieved {len(configs)} active bot config(s).")
+            self.logger.debug(configs)
+            return configs
+        except Exception as e:
+            self.logger.error(f"Failed to fetch bot configs: {e}")
+            return []
 
 
 
 if __name__ == "__main__":
 
-    conn = pyodbc.connect(connection_string)
-    SQL_QUERY = """
-    SELECT * FROM [binance-bot-db].bnb.bot_configs WHERE enabled=1;
-    """
-    cursor = conn.cursor()
-    cursor.execute(SQL_QUERY)
-    records = cursor.fetchall()
-    for r in records:
-        print(r)
+    # conn = pyodbc.connect(connection_string)
+    # SQL_QUERY = """
+    # SELECT * FROM [binance-bot-db].bnb.bot_configs WHERE enabled=1;
+    # """
+    # cursor = conn.cursor()
+    # cursor.execute(SQL_QUERY)
+    # records = cursor.fetchall()
+    # for r in records:
+    #     print(r)
+    az = AzureSQLAdapter()
+    az.fetch_bot_configs()
 
     print('Bye!')
 
