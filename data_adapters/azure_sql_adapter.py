99a9ead36e5a82de3194998b9338825bba9602a3
ntpk <natthaphat.kiti@gmail.com>
2025-06-16 21:44:26 +0700

    ⚡ add fetch bot position

--- data_adapters/azure_sql_adapter.py
+++ data_adapters/azure_sql_adapter.py
@@ -15,19 +15,20 @@ password = os.getenv("AZURE_SQL_DB_ADMIN_PASSWORD")
 
 connection_string = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
 
+
 class AzureSQLAdapter(BaseAdapter):
     def __init__(self):
         self.logger = CustomLogger(name=self.__class__.__name__)
 
         try:
             self.logger.debug("Attempting to connect to Azure SQL database.")
-            self.connection_string = os.getenv("AZURE_SQL_CONN") or self.build_conn_string()
+            self.connection_string = os.getenv(
+                "AZURE_SQL_CONN") or self.build_conn_string()
             self.conn = pyodbc.connect(self.connection_string)
             self.logger.debug("Successfully connected to Azure SQL database.")
         except Exception as e:
             self.logger.error(f"Failed to connect to Azure SQL: {e}")
             raise
-
 
     def build_conn_string(self):
         server = os.getenv("AZURE_SQL_DB_SERVER", 'Not Set')
@@ -36,27 +37,56 @@ class AzureSQLAdapter(BaseAdapter):
         password = os.getenv("AZURE_SQL_DB_ADMIN_PASSWORD", 'Not Set')
 
         if any(val == 'Not Set' for val in [server, database, username, password]):
-            raise OSError("Missing one or more Azure SQL DB environment variables.")
-        
-        return f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+            raise OSError(
+                "Missing one or more Azure SQL DB environment variables.")
 
+        return f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
 
     def fetch_bot_configs(self) -> list:
         try:
-            self.logger.debug("Fetching active bot configs from Azure SQL Server...")
+            self.logger.debug(
+                "Fetching active bot configs from Azure SQL Server...")
             cursor = self.conn.cursor()
 
-            cursor.execute("SELECT * FROM [binance-bot-db].bnb.bot_configs WHERE enabled=1;")
+            sql = "SELECT * FROM [binance-bot-db].bnb.bot_configs WHERE enabled = ?;"
+            cursor.execute(sql, (1))
+
             columns = [column[0] for column in cursor.description]
             rows = cursor.fetchall()
             configs = [dict(zip(columns, row)) for row in rows]
-            self.logger.debug(f"Retrieved {len(configs)} active bot config(s).")
+
+            self.logger.debug(
+                f"Retrieved {len(configs)} active bot config(s).")
             self.logger.debug(configs)
+
             return configs
         except Exception as e:
             self.logger.error(f"Failed to fetch bot configs: {e}")
             return []
 
+    def fetch_bot_position(self, bot_name: str) -> list:
+        try:
+            self.logger.debug("Fetching bot position from Azure SQL Server...")
+            cursor = self.conn.cursor()
+
+            sql = "SELECT * FROM [binance-bot-db].bnb.bot_positions WHERE position_id = ?;"
+            cursor.execute(sql, (bot_name.upper()))
+            columns = [column[0] for column in cursor.description]
+            rows = cursor.fetchall()
+            positions = [dict(zip(columns, row)) for row in rows]
+
+            self.logger.debug(
+                f"Retrieved {len(positions)} bot position.")
+            if len(positions) > 1:
+                self.logger.warning(
+                    'Bot position should not exceed 1 position at any moment')
+
+            self.logger.debug(positions)
+
+            return positions
+        except Exception as e:
+            self.logger.error(f"Failed to fetch bot position: {e}")
+            return []
 
 
 if __name__ == "__main__":
