9770436f5ef4068bce132d902f9f2139291c45bc
ntpk <natthaphat.kiti@gmail.com>
2025-06-18 05:04:59 +0700

    🎨 print summary table at end of execution

--- commons/common.py
+++ commons/common.py
@@ -5,43 +5,42 @@ def get_datetime_now_string_gmt_plus_7():
     dt_str = dt.strftime('%Y-%m-%d %H:%M:%S')
     return dt_str
 
-def format_duration(ms: int) -> str:
-    """Convert milliseconds to a human-readable duration string"""
-    seconds = ms // 1000
-    minutes, sec = divmod(seconds, 60)
-    hours, min_ = divmod(minutes, 60)
-    days, hr = divmod(hours, 24)
+
+def format_duration_minutes(minutes: int) -> str:
+    days = minutes // (24 * 60)
+    hours = (minutes % (24 * 60)) // 60
+    mins = minutes % 60
 
     parts = []
     if days > 0:
         parts.append(f"{days}d")
-    if hr > 0:
-        parts.append(f"{hr}h")
-    if min_ > 0:
-        parts.append(f"{min_}m")
-    if sec > 0 or not parts:
-        parts.append(f"{sec}s")
+    if hours > 0:
+        parts.append(f"{hours}h")
+    if mins > 0 or not parts:
+        parts.append(f"{mins}m")
 
     return " ".join(parts)
 
-def print_result_table(results: list[dict]):
+
+def print_result_table(bots_run: list[dict]):
     # Define header
-    header = f"| {'Bot':<40} {'Duration':<20} {'Trades':<8} {'Win Rate':<10} {'Initial':<10} {'Final':<10} {'ROI':<11} {'Daily ROI':<11} {'Auunal ROI':<8} |"
+    header = f"| {'Bot':<40} {'Duration':<20} {'Position':<8} {'Win Rate':<10} {'Initial':<10} {'Final':<10} {'ROI':<11} {'Daily ROI':<11} {'Auunal ROI':<8} |"
     print("\n" + "-" * len(header))
     print(header)
     print("-" * len(header))
 
 
     # Print each row
-    for r in results:
+    for r in bots_run:
+        duration_str = format_duration_minutes(r['duration_minutes'])
         print(
             f"| {r['bot_fullname']:<40} "
-            f"{str(r['duration']):<20} "
-            f"{r['total_trades']:<8} "
+            f"{str(duration_str):<20} "
+            f"{r['total_positions']:<8} "
             f"{r['win_rate']:.2f} %{' ' * (11 - len(f'{r['win_rate']:.2f} %'))}"
             f"${r['initial_balance']:<9.2f} "
             f"${r['final_balance']:<9.2f} "
-            f"{r['roi']:.2f} %{' ' * (12 - len(f'{r['roi']:.2f} %'))}"
+            f"{r['roi_percent']:.2f} %{' ' * (12 - len(f'{r['roi_percent']:.2f} %'))}"
             f"{r['daily_roi']:.2f} %{' ' * (12 - len(f'{r['daily_roi']:.2f} %'))}"
             f"{r['annual_roi']:.2f} %{' ' * (10 - len(f'{r['annual_roi']:.2f} %'))} |"
             # f"{r['roi']:<8.2f}% "
