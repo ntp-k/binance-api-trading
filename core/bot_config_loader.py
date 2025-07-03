83fb174e37a616d2ba373c057d922960d1fc6686
ntpk <natthaphat.kiti@gmail.com>
2025-07-04 00:55:46 +0700

    ⚡ encode UTF8

--- core/bot_config_loader.py
+++ core/bot_config_loader.py
@@ -3,7 +3,7 @@ import json
 from models.bot_config import BotConfig
 
 def load_config(file_path: str) -> list[BotConfig]:
-    with open(file=file_path, mode="r") as f:
+    with open(file=file_path, mode="r", encoding="utf-8") as f:
         raw = json.load(fp=f)
     return [BotConfig.from_dict(data=item) for item in raw]
 
